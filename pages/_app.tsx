5062e8d9d975e32f753fec3d64508fff5b087ee3
matkot11 <matkot11@gmail.com>
2023-02-11 18:28:01 +0100

    Add variables and install font family

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import "@/styles/globals.scss";
+import "@/styles/variables.scss"
+import type {AppProps} from "next/app";
+import {Montserrat, K2D} from '@next/font/google'
+import classNames from "classnames";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+const montserrat = Montserrat({
+    subsets: ["latin"]
+})
+const k2d = K2D({
+    subsets: ["latin"],
+    weight: "600"
+})
+
+export default function App({Component, pageProps}: AppProps) {
+    return (
+        <main className={classNames(montserrat.className, k2d.className)}>
+            <Component {...pageProps} />
+        </main>
+    )
 }
